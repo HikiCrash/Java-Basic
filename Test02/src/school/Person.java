ffcca342f2182f9b742432d780697eed0307b8b6
HikiCrash <csh02127@naver.com>
2026-04-22 20:02:30 +0900

    Person.java add

---
+++ Test02/src/school/Person.java
@@ -0,0 +1,9 @@
+package school;
+
+public class Person {
+    public String name;
+    public double height;
+    public double weight;
+    public char gender;
+    public boolean married;
+} // class로 만든 자료형 -> 대문자
